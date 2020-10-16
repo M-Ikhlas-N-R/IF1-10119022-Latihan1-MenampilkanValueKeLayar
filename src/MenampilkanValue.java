305d8d7d0e6159409fbaf21f9aca465c65cadc22
M Ikhlas N R <72897730+M-Ikhlas-N-R@users.noreply.github.com>
2020-10-16 09:17:09 +0700

    upload file

---
+++ src/MenampilkanValue.java
@@ -0,0 +1,21 @@
+/*
+ * @author
+ * Nama              : M Ikhlas N R
+ * Kelas             : IF-1
+ * NIM               : 10119022
+ * Deskripsi Program : Menampilakan Value Ke Layar
+ *
+ */
+public class MenampilkanValue {
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "-------------------");
+        System.out.println("NAMA\t\t: Rizki Adam Kurniawan");
+        System.out.println("ALAMAT\t\t: Jalan Antapani no 99");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 25 Januari 1991");
+        System.out.println("-------------------");
+        System.out.println("HOBI\t\t: Renang & Lari");
+        System.out.println("MAKES\t\t: Sate");
+        System.out.println("MIKES\t\t: Cendol");
+    }
+}
